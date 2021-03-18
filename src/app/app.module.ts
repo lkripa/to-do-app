bb5b3f225826174277b0d2303d2b8eded41778f4
lkripa <lara@lkripa.com>
2021-03-18 15:19:31 +0100

    initialize base files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListBodyComponent } from './list-body/list-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListBodyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/list-body/list-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-body',
+  templateUrl: './list-body.component.html',
+  styleUrls: ['./list-body.component.css']
+})
+export class ListBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
